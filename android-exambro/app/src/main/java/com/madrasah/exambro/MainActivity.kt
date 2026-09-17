89bd68dcf14e4343755825a00c003054862789b7
Syamsul Bahri <syamsulbahri.agro27b@gmail.com>
2026-09-17 17:26:16 +0800

    fix: gunakan setup-gradle langsung di workflow CI

--- android-exambro/app/src/main/java/com/madrasah/exambro/MainActivity.kt
+++ android-exambro/app/src/main/java/com/madrasah/exambro/MainActivity.kt
@@ -266,6 +266,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     // 5. BLOKIR TOMBOL KEMBALI: Butuh PIN Pengawas untuk Keluar
+    @Suppress("DEPRECATION")
     @Deprecated("Deprecated in Java")
     override fun onBackPressed() {
         promptProctorPinToExit()
